7d75e3e74496f45ff1d1894f07367ca0806cd70b
nguyen.huu.nam <proman112111@gmail.com>
2019-03-29 10:09:22 +0700

    Add Test Library

---
+++ library/src/main/java/com/namnh/emptylib/TestImportAAR.kt
@@ -0,0 +1,8 @@
+package com.namnh.emptylib
+
+import com.mux.stats.sdk.core.model.CustomerPlayerData
+
+class TestImportAAR {
+    private val test: CustomerPlayerData? = null
+    // OK It's work
+}
