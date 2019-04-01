e4c1f238299d28b6ea687e91f2308e0f0ebf566d
nguyen.huu.nam <proman112111@gmail.com>
2019-04-01 16:31:56 +0700

    Release 4.0.0

--- library/src/main/java/com/namnh/emptylib/TestImportAAR.kt
+++ library/src/main/java/com/namnh/emptylib/TestImportAAR.kt
@@ -5,4 +5,8 @@ import com.mux.stats.sdk.core.model.CustomerPlayerData
 class TestImportAAR {
     private val test: CustomerPlayerData? = null
     // OK It's work
+
+    fun test(): Int {
+        return 3 + 4
+    }
 }
---
+++ library2/src/main/java/com/namnh/library2/TestUsingLib1.kt
@@ -0,0 +1,9 @@
+package com.namnh.library2
+
+import com.namnh.emptylib.TestImportAAR
+
+class TestUsingLib1 {
+    fun main() {
+        println(TestImportAAR().test())
+    }
+}
